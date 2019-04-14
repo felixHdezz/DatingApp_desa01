08d12d87e90146f7b7dec409f78cd57871630ac3
FelixHdezz <felix_hdezz95@hotmail.com>
2019-04-14 01:52:34 -0500

    add config datatbase

--- AppBackEnd/config/database.js
+++ AppBackEnd/config/database.js
@@ -1,6 +1,10 @@
 // config/database.js
 module.exports = {
-
-    'urlmongo' : 'mongodb://127.0.0.1:27017/'
-
+    urlmongo : 'mongodb://127.0.0.1:27017/',
+    configSQL = {
+        user: 'sa',
+        password: '',
+        server: 'MSSQLSERVER01', 
+        database: 'DattingAPP' 
+    }
 };
