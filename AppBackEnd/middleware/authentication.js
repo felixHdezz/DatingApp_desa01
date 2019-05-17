a1de16f8a8aa76f6305b96202dba6e54ec22a36b
FelixHdezz <felix_hdezz95@hotmail.com>
2019-05-16 23:06:59 -0500

    update middleware

--- AppBackEnd/middleware/authentication.js
+++ AppBackEnd/middleware/authentication.js
@@ -1,25 +1,49 @@
 'use strict'
+
+//carga todos modulos que se necesitan
 var jwt = require('jwt-simple');
 var moment  = require('moment');
-var secret = 'my_voice_is_my_password';
+var _execQuery = require('../database/query');
+var debuger = require('../config/debugerPrint');
 
-exports.ensureAuth = function(req, res, next){
-    if(!req.headers.authorization){
-        res.status(403).send({message:'La peticion no tiene la cabecera de autentificacion'});
-    }else{
-        var _token = req.headers.authorization.replace(/['"]+/g,'');
+//Carga los modelos necesarios
+var _token_model = require('../models/token');
+
+//Clave generica secreta
+var _secret = 'my_voice_is_my_password';
+
+exports.ensureAuth = function(_req, _res, next){
+    if(!_req.headers.authorization){
+        _res.status(403).send({message:'Error authentication user'});
+    } else {
+        var _token = _req.headers.authorization.replace(/['"]+/g,'');
         try{
-            var _payload = jwt.decode(_token, secret);
-            if(_payload <= moment().unix()){
-                return res.status(401).send({message:'El token ha expirado'});
+            var _payload = jwt.decode(_token, _secret);
+            //Obtiene los datos de usuario en la base de datos para validar si el token ha expirado
+            var _prmFind = { user: _payload.sub };
 
-            }
+            _execQuery.findOne(_token_model, _prmFind, null, null, null, function(_error, _data){
+                if(_error){
+                    debuger.debugPrint(_error, null);
+                }else{
+                    if(_data.length > 0){
+                        //Obtiene los datos de usuario
+                        var _dToken = _data[0];
+                        
+                        //Valida si el token ya esta expirado
+                        if(_dToken.exp <= moment().unix()){
+                            return _res.status(401).send({message:'Token expired'});
+                        }
+                    }
+                }
+            });
         }catch(ex){
-            console.log(ex);
-            return res.status(404).send({message:'Token no valido'});
+            debuger.debugPrint(ex, null);
+            return _res.status(404).send({message:'Token is not valided'});
         }
     }
-    req.user = _payload;
+    _res.user = _payload;
+
+    //Next continue application
     next();
 };
-
