f9deabb5ed50a7c0df154ed233d261d5d11cb27a
FelixHdezz <felix_hdezz95@hotmail.com>
2019-05-29 22:13:54 -0500

    update passport-facebook.js

--- AppBackEnd/auth/passport-facebook.js
+++ AppBackEnd/auth/passport-facebook.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var User = mongoose.model('User');
+var User = require('../models/user');
 var FacebookStrategy = require('passport-facebook').Strategy;
 
 module.exports = function(_passport){
