6650b4b7ae33e6ab4d838a97af3ce89e99ec5d5a
FelixHdezz <felix_hdezz95@hotmail.com>
2019-05-31 00:06:53 -0500

    update files

--- AppBackEnd/config/authExternal.js
+++ AppBackEnd/config/authExternal.js
@@ -3,9 +3,9 @@
 // expose our config directly to our application using module.exports
 module.exports = {
     'googleAuth' : {
-        'clientID'         : 'clientID',
-        'clientSecret'     : 'clientSecret',
-        'callbackURL'      : 'urlcallback'
+        'clientID'         : '634127625828-mc8ttvqfu2m3bbc8h0bu9f40a9f4vi0k.apps.googleusercontent.com',
+        'clientSecret'     : 'yJaMhzkyr1mMQW_cT_-oCFS4',
+        'callbackURL'      : 'http://localhost:3011/api/auth/google/callback'
     },
     'facebookAuth': {
         'clientID'         : 'clientID',
