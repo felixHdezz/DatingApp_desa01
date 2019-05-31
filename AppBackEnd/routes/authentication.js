668d71e6d93cf9447c0847d33e709a7a099aa341
FelixHdezz <felix_hdezz95@hotmail.com>
2019-05-30 23:25:47 -0500

    update authentification.js

--- AppBackEnd/routes/authentication.js
+++ AppBackEnd/routes/authentication.js
@@ -9,7 +9,29 @@ var multipart = require('connect-multiparty');
 // Manejo de rutas en NodeJS
 var api = express.Router();
 
-// Define las rutas para el controlador de autentificacion
-api.post('/auth/v1/token', authenticationController.authentication);
+module.exports = function(passport){
 
-module.exports = api;
+    // Define las rutas para el controlador de autentificacion
+    api.post('/auth/v1/token', authenticationController.authentication);
+
+    // =============================================================================
+    // AUTHENTICATE (FIRST LOGIN) ==================================================
+    // =============================================================================
+    // google ---------------------------------
+    // send to google to do the authentication
+    api.get('/auth/google', passport.authenticate('google', {
+        hd: 'email',
+        scope: ['profile', 'email']
+    }));
+
+    // the callback after google has authenticated the user
+    api.get('/auth/google/callback',
+        passport.authenticate('google'),
+        (req, res)=> {
+            var _data = req.user;
+            res.status(200).send({ _data });
+        }
+    );
+
+    return api;
+};
