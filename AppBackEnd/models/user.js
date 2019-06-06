2c3629e5bddc5008cb03354821ce039145a842c1
FelixHdezz <felix_hdezz95@hotmail.com>
2019-06-05 23:59:08 -0500

    update user.js

--- AppBackEnd/models/user.js
+++ AppBackEnd/models/user.js
@@ -7,25 +7,39 @@ var Schema = mongoose.Schema;
 
 // Esquema de modelo de datos para la tabla de usuarios
 var UserSchema = Schema({
-			userId: Number,
-			name: String,
-			lastname: String,
-			description: String,
-			sex: { type: Schema.ObjectId, ref: 'Sex' },
-			birthday: Date,
-			email: String,
-			position: String,
-			company: String,
-			school: String,
-			livingin: String,
-			city: { type: Schema.ObjectId, ref: 'City'},
-			lat1: String,
-			lat2: String,
-			profilePic: String,
-			isDelete: Number,
-			createdat: Date,
-			lastmodified:Date,
-			deletedat: Date
+	userId: Number,
+	name: String,
+	lastname: String,
+	description: String,
+	sex: { type: Schema.ObjectId, ref: 'Sex' },
+	birthday: Date,
+	email: String,
+	position: String,
+	company: String,
+	school: String,
+	livingin: String,
+	city: { type: Schema.ObjectId, ref: 'City'},
+	lat1: String,
+	lat2: String,
+	profilePic: String,
+	isDelete: Number,
+	createdat: Date,
+	lastmodified:Date,
+	deletedat: Date,
+	facebook: {
+		id: String,
+		access_token: String,
+		firstName: String,
+		lastName: String,
+		email: String
+	},
+	google: {
+		id: String,
+		token: String,
+		username: String,
+		displayName: String,
+		lastStatus: String
+	}
 });
 
 // Exporta el modelo
